8668c2278d95de77824ffd6cd3b0c7e68d5d2dcb
opsource <v.opsource@gmail.com>
2022-05-01 18:34:41 +0430

    push vendor files

--- public/index.php
+++ public/index.php
@@ -1,18 +1,19 @@
 <?php
+/*
+ * require composer autoload
+ */
 require_once __DIR__.'/../vendor/autoload.php';
-ini_set('display_errors', 'On');
 use app\core\base\App;
+/*
+ * require base app class for use current dir
+ */
 $app = new App(dirname(__DIR__));
+
+/*
+ * require web routers
+ */
 include_once __DIR__.'/../routers/web_route.php';
 $app->run();
-//namespace vendor;
-//require_once 'Vendor/DBConnection.php';
-//require_once 'Vendor/Bootstrap.php';
-//ini_set('display_errors', 'On');
-//$instance = DBConnection::getInstance();
-//$instance->create("INSERT INTO records (session_id) VALUES (?)", [Bootstrap::getSessionKey()]);
-//echo json_encode($instance->select("SELECT * FROM records;"));
-
 
 
 
